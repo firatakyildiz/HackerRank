7e86764089c90398fd4448e026e5bf105fc2c2da
Fırat Akyıldız <akyildiz.firat@gmail.com>
2022-12-03 18:30:26 +0300

    Project Euler 6th question

---
+++ HackerRank/ProjectEuler/6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.ProjectEuler
+{
+    /// <summary>
+    /// Link : https://www.hackerrank.com/contests/projecteuler/challenges/euler005/problem
+    /// Difficulty : Easy
+    /// Solution : Simple calculation
+    /// </summary>
+    public static class _6
+    {
+        public static void Solve()
+        {
+            var numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < numberOfTestCases; i++)
+            {
+                var number = Convert.ToInt32((Console.ReadLine()));
+                
+                var sumOfSquares = 0L;
+                for (var j = 1; j <= number; j++)
+                {
+                    sumOfSquares += j * j;
+                }
+
+                var squareOfTheSum = (long)Math.Pow((number * (number + 1)) / 2, 2);
+                
+                Console.WriteLine(Math.Abs(sumOfSquares - squareOfTheSum));
+            }
+        }
+    }
+}
