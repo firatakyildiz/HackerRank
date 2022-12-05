ca10ba069952d68b58ffc10471dfe9aafbba9a7e
Fırat Akyıldız <akyildiz.firat@gmail.com>
2022-12-06 02:30:14 +0300

    One week preparation kit, day 2 : counting sort and mock test

---
+++ HackerRank/OneWeekPreparationKit/Day2/CountingSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.OneWeekPreparationKit.Day2
+{
+    /// <summary>
+    /// Link : https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem
+    /// Difficulty : Easy
+    /// Solution : Simple calculation
+    /// </summary>
+    public static class CountingSort
+    {
+        public static void Solve()
+        {
+            var n = Convert.ToInt32(Console.ReadLine().Trim());
+            var arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            var countingSortArr = new int[100];
+            foreach (var i in arr)
+            {
+                countingSortArr[i]++;
+            }
+            Console.WriteLine(string.Join(' ', countingSortArr));
+        }
+    }
+}
---
+++ HackerRank/OneWeekPreparationKit/Day2/MockTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.OneWeekPreparationKit.Day2
+{
+    /// <summary>
+    /// If we examine the question, we can see that each number can only be moved to at most 4 different places.
+    /// In addition, all the numbers can be moved around individually like a Rubik's Cube.
+    /// So we only check the largest number that can be placed in each location.
+    /// </summary>
+    internal class MockTest
+    {
+        public static int flippingMatrix(List<List<int>> matrix)
+        {
+            var size = matrix.Count;
+            var total = 0;
+            for (var i = 0; i < size / 2; i++)
+            for (var j = 0; j < size / 2; j++)
+            {
+                var candidate1 = matrix[i][j];
+                var candidate2 = matrix[i][size - 1 - j];
+                var candidate3 = matrix[size - 1 - i][j];
+                var candidate4 = matrix[size - 1 - i][size - 1 - j];
+                total += Math.Max(Math.Max(candidate1, candidate2), Math.Max(candidate3, candidate4));
+            }
+            return total;
+        }
+    }
+}
