0be6ec14ecf6c3094447c831737aa0ab0cddf80c
Fırat Akyıldız <akyildiz.firat@gmail.com>
2022-12-06 01:59:03 +0300

    One week preparation kit, day 2 diagonal difference

---
+++ HackerRank/OneWeekPreparationKit/Day2/DiagonalDifference.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.OneWeekPreparationKit.Day2
+{
+    /// <summary>
+    /// Link : https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem
+    /// Difficulty : Easy
+    /// Solution : Simple calculation
+    /// </summary>
+    public static class DiagonalDifference
+    {
+        public static void Solve()
+        {
+            var n = Convert.ToInt32(Console.ReadLine().Trim());
+            var arr = new List<List<int>>();
+
+            for (var i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            var firstDiagonalTotal = 0;
+            var secondDiagonalTotal = 0;
+            for (var i = 0; i < n; i++)
+            {
+                firstDiagonalTotal += arr[i][i];
+                secondDiagonalTotal += arr[i][n - i - 1];
+            }
+            Console.WriteLine(Math.Abs(firstDiagonalTotal - secondDiagonalTotal));
+        }
+    }
+}
