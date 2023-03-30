732dbebfd9e4de551c4043b39ece3839475fd0d0
Fırat Akyıldız <akyildiz.firat@gmail.com>
2023-03-30 19:16:43 +0300

    Interview Preparation Kit : Count Trilets and Frequency Queries

---
+++ HackerRank/InterviewPreparationKit/DictionariesAndHashmaps/CountTriplets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.InterviewPreparationKit.DictionariesAndHashmaps
+{
+    internal class CountTriplets
+    {
+        public static long countTriplets(List<long> arr, long r)
+        {
+            var singles = new Dictionary<long, long>();
+            var doubles = new Dictionary<long, long>();
+            var tripletsCount = 0L;
+            foreach (var number in arr)
+            {
+                var candidate = number / r;
+
+                // if the result of the division is not an integer, disregard it
+                if (candidate * r == number)
+                {
+                    // this means we have found triplets
+                    if (doubles.ContainsKey(candidate))
+                        tripletsCount += doubles[candidate];
+
+                    // whenever a new element is read from the array, if we had read a number that is r times this one,
+                    // keep track of this too, in another hashmap
+                    if (singles.ContainsKey(candidate))
+                    {
+                        if (doubles.ContainsKey(number))
+                            doubles[number] += singles[candidate];
+                        else
+                            doubles[number] = singles[candidate];
+                    }
+                }
+
+                // put each element into the hashmap, this keeps count of base(first of triplets) elements
+                if (singles.ContainsKey(number))
+                    singles[number]++;
+                else
+                    singles[number] = 1;
+            }
+            return tripletsCount;
+        }
+    }
+}
---
+++ HackerRank/InterviewPreparationKit/DictionariesAndHashmaps/FrequencyQueries.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.InterviewPreparationKit.DictionariesAndHashmaps
+{
+    /// <summary>
+    /// Keep individual number frequencies in a hashtable. Keep numbers grouped by frequency in another hashtable.
+    /// </summary>
+    class Solution
+    {
+        private static Dictionary<int, List<int>> frequencyGroups = new Dictionary<int, List<int>>();
+        private static Dictionary<int, int> individualFrequencies = new Dictionary<int, int>();
+
+        private static void changeFrequency(int input, int oldFrequency, int newFrequency)
+        {
+            individualFrequencies[input] = newFrequency;
+            if(oldFrequency != 0)
+                frequencyGroups[oldFrequency].Remove(input);
+            if (!frequencyGroups.ContainsKey(newFrequency))
+                frequencyGroups[newFrequency] = new List<int>();
+            frequencyGroups[newFrequency].Add(input);
+        }
+        private static void insert(int input)
+        {
+            if (individualFrequencies.ContainsKey(input))
+            {
+                var previousFrequency = individualFrequencies[input];
+                var newFrequency = previousFrequency + 1;
+                changeFrequency(input, previousFrequency, newFrequency);
+            }
+            else
+            {
+                changeFrequency(input, 0, 1);
+            }
+        }
+
+        private static void remove(int input)
+        {
+            if (individualFrequencies.ContainsKey(input))
+            {
+                var frequency = individualFrequencies[input];
+                if(frequency > 1)
+                    changeFrequency(input, frequency, frequency - 1);
+                else
+                {
+                    individualFrequencies.Remove(input);
+                    frequencyGroups[frequency].Remove(input);
+                }
+            }
+        }
+
+        private static bool check(int input)
+        {
+            if (frequencyGroups.ContainsKey(input))
+                return frequencyGroups[input].Count > 0;
+            return false;
+        }
+
+        // Complete the freqQuery function below.
+        static List<int> freqQuery(List<List<int>> queries)
+        {
+            var result = new List<int>();
+            foreach (var query in queries)
+            {
+                var operation = query[0];
+                var value = query[1];
+                
+                switch (operation)
+                {
+                    case 1:
+                        insert(value);
+                        break;
+                    case 2:
+                        remove(value);
+                        break;
+                    case 3:
+                        result.Add(check(value) ? 1 : 0);
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
